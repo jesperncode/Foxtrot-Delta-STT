a9b08b3216f5c14c9c0246929c04861079967c5b
Test Testersen <jesperkx@gmail.com>
2026-01-22 10:43:39 +0100

    fix

--- app/utils/meeting.py
+++ app/utils/meeting.py
@@ -1,15 +1,19 @@
 import uuid
 from datetime import datetime
+from pathlib import Path
+
 from app.utils.storage import get_meetings_dir
+
 
 def create_meeting():
     meeting_id = str(uuid.uuid4())
-    meeting_dir = get_meetings_dir() / meeting_id
-    meeting_dir.mkdir()
+    meeting_dir: Path = get_meetings_dir() / meeting_id
+    meeting_dir.mkdir(parents=True, exist_ok=False)
 
     (meeting_dir / "created_at.txt").write_text(
         datetime.now().isoformat(),
-        encoding="utf-8"
+        encoding="utf-8",
     )
 
     return meeting_id, meeting_dir
+
